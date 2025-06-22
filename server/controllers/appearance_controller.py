9f91decd72787a0c49ef8109d7ea2fbaaa7ef171
nevil <nevil.oporo@student.moringaschool.com>
2025-06-22 23:21:44 +0300

    adding the controllers

---
+++ server/controllers/appearance_controller.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required
+from server.models.appearance import Appearance
+from server.models import db
+
+appearance_bp = Blueprint("appearances", __name__)
+
+@appearance_bp.route("/appearances", methods=["POST"])
+@jwt_required()
+def create_appearance():
+    data = request.get_json()
+
+    try:
+        appearance = Appearance(
+            rating=data["rating"],
+            guest_id=data["guest_id"],
+            episode_id=data["episode_id"]
+        )
+        db.session.add(appearance)
+        db.session.commit()
+
+        return jsonify({
+            "id": appearance.id,
+            "rating": appearance.rating,
+            "guest_id": appearance.guest_id,
+            "episode_id": appearance.episode_id
+        }), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
